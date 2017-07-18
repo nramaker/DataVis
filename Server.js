4d3668854fe63f1743d7b2d53298ba571d642ba7
Nathan Ramaker <Nathan.ramaker@helpsystems.com>
2017-07-18 12:28:33 -0500

    navigation

--- Server.js
+++ Server.js
@@ -12,6 +12,19 @@ router.get("/",function(req,res){
   res.sendFile(path + "index.html");
 });
 
+router.get("/page2",function(req,res){
+  res.sendFile(path + "p2.html");
+});
+router.get("/page3",function(req,res){
+  res.sendFile(path + "p3.html");
+});
+router.get("/page4",function(req,res){
+  res.sendFile(path + "p4.html");
+});
+router.get("/page5",function(req,res){
+  res.sendFile(path + "p5.html");
+});
+
 router.get("/about",function(req,res){
   res.sendFile(path + "about.html");
 });
